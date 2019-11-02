fbf798e547aa786266a332871a0177c0e804a661
Roque Francisco <roquefsd@gmail.com>
2019-11-01 23:11:19 -0300

    Update Fitter.js

--- Fitter.js
+++ Fitter.js
@@ -22,7 +22,7 @@ class Fitter {
             el = typeof this.options.element === 'string' ? window.document.querySelector(this.options.element) : this.options.element;
         }
         
-        el.parentNode.style.border = '1px solid red';
+        el.style.border = '1px solid red';
 
         let style = window.getComputedStyle(el, null);
         let fontSize = style.getPropertyValue('font-size');
@@ -39,7 +39,7 @@ class Fitter {
                 let r = this.getTextWidth(el.innerText, `${fontWeight} ${c}px ${fontFamily}`);
                 if (r > el.clientWidth) {
                     el.style.fontSize = `${c - 1 > this.options.max ? this.options.max : c - 1}px`;
-                    el.parentNode.style.border = '1px solid black';
+                    el.style.border = '1px solid black';
                     return;
                 }
             }
@@ -51,12 +51,12 @@ class Fitter {
                 let r = this.getTextWidth(el.innerText, `${fontWeight} ${c}px ${fontFamily}`);
                 if (r > el.clientWidth) {
                     el.style.fontSize = `${c - 1 < this.options.min ? this.options.min : c - 1}px`;
-                    el.parentNode.style.border = '1px solid black';
+                    el.style.border = '1px solid black';
                     return;
                 }
             }
             el.style.fontSize = `${this.options.min}px`;
-            el.parentNode.style.border = '1px solid black';
+            el.style.border = '1px solid black';
             return;
         }
     }
