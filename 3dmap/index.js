63acd7c56c9274c151532eea2af32db289743653
Roque Francisco <roquefsd@gmail.com>
2021-03-03 18:57:47 -0300

    round

--- 3dmap/index.js
+++ 3dmap/index.js
@@ -30,8 +30,7 @@ loader.load(
         controls.update();
     },
     function (xhr) {
-        console.log(xhr.loaded, xhr.total);
-        document.querySelector('.js-value').innerHTML = ((xhr.loaded / 13933416 * 100) + '% loaded');
+        document.querySelector('.js-value').innerHTML = ((xhr.loaded / 13933416 * 100).toFixed(2) + '% loaded');
     },
     function (error) {
         document.querySelector('.js-value').innerHTML = error;
